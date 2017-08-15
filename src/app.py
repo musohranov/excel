89071ab6076ea968f9a22b80d54fb8718b8a1b69
musohranov <andrenius@sibmail.ru>
2017-08-16 01:24:23 +0700

    Инструкция и примеры вычислений.

--- src/app.py
+++ src/app.py
@@ -15,7 +15,6 @@ def run():
         sheet.add_line(input())
 
     result = sheet.calculate()
-    print('\n')
 
     for y in range(0, sheet.get_size().y):
         print('\t'.join([str(result[(x + 1, y + 1)]) for x in range(0, sheet.get_size().x)]))
